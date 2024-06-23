22580dc55ccca03b6ee643187dd666a0e0a3c0c4
Franco Javier Morales <francomoralew36@gmail.com>
2024-06-23 17:46:44 -0300

    "Nuevos Ejercicios"

---
+++ Python/Ejercicio8.py
@@ -0,0 +1,7 @@
+condicion = True
+# if condicion:
+#    print("Condicion Verdadera")
+# else:
+#   print("Condicion Falsa")
+#
+print("Condicion Verdadera") if condicion else print("Condicion Falsa")
